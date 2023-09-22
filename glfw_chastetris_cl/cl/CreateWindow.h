6f1e473277c435dc6450f2779c7561d22611e0fa
Chastity White Rose <chastitywhiterose@gmail.com>
2023-09-22 01:45:00 -0500

    Add files via upload

--- glfw_chastetris_cl/cl/CreateWindow.h
+++ glfw_chastetris_cl/cl/CreateWindow.h
@@ -3,15 +3,11 @@
  The goal is to reduce the size of the main.c file and clean up everything.
 */
 
-
-
 /*function only for printing errors*/
 void error_callback(int error,const char *description)
 {
  fprintf(stderr,"Error: %s\n",description);
 }
-
-
 
 int CreateWindow(int width,int height)
 {
@@ -21,7 +17,6 @@ int CreateWindow(int width,int height)
   
  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,2); /*Using version 2.1 of OpenGL*/
  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,1);
-
  glfwWindowHint(GLFW_DOUBLEBUFFER,GLFW_FALSE); /*single buffered window*/
 
  window=glfwCreateWindow(width,height,"OpenGL Legacy",NULL,NULL);
@@ -34,9 +29,11 @@ int CreateWindow(int width,int height)
 
   glfwMakeContextCurrent(window);
   printf("The OpenGL version is: %s\n",glGetString(GL_VERSION));
-  /*2D orthographic matrix*/
 
-  glOrtho(0,width,height,0,-1,1);
+  /*2D orthographic matrix*/
+  /*the following line commented out. Only for historical purposes.*/
+  /*glOrtho(0,width,height,0,-1,1);*/
+
   glClearColor(0.0,0.0,0.0,1.0); /*color used to clear the window*/
 
  return 0;
--- glfw_chastetris_cl/cl/clRect.h
+++ glfw_chastetris_cl/cl/clRect.h
@@ -3,29 +3,63 @@ clrect.h
 
 This header file is all about drawing filled rectangles.
 The idea is to be able to switch out which function is used by renaming it.
-For example, glRecti is a real function that exists in OpenGL.
-Whichever of my function is named clRecti at the time will be used by the program I am running.
+For example, glRectf is a real function that exists in OpenGL.
+
+The problem arises from the fact that OpenGL commands take floating point arguments which must be in the range -1 to +1. Additionally, 0 is in the middle of the screen for X and Y coordinates. To make OpenGL compatible with the same coordinate system used in SDL, I have made a collection of functions which convert screen pixel coordinates to OpenGL coordinates. This means that I am no longer dependent on the "glOrtho" any more. It also means I have greater control of the fixed function pipeline than I ever have before.
+
+Whichever of my function is named clRectf at the time will be used by the program I am running.
 This means that I can change one function and control the output every time the program attempts to draw a rectangle.
 It also means I can then port this function over to other graphics library to make an OpenGL emulator.
 However, I am limited to 2D graphics because that is all I care about.
-
 */
+
+float clx(float f)
+{
+ f=f*2/width-1; /*printf("X=%f\n",f);*/
+ return f;
+}
+
+float cly(float f)
+{
+ f=f*2/height-1; /*printf("Y=%f\n",f);*/
+ return -f;
+}
+
+void clVertex2f(float x,float y)
+{
+ glVertex2f( clx(x) , cly(y) );
+}
 
 /*draw rectangle by calling defining and drawing a polygon with 4 vertices*/
 
-void clRecti( GLint x1, GLint y1, GLint x2, GLint y2 )
+void clRectf( float x1, float y1, float x2, float y2 )
 {
  glBegin(GL_POLYGON);
- glVertex2i(x1, y1);
- glVertex2i(x2, y1);
- glVertex2i(x2, y2);
- glVertex2i(x1, y2);
+ clVertex2f(x1, y1);
+ clVertex2f(x2, y1);
+ clVertex2f(x2, y2);
+ clVertex2f(x1, y2);
  glEnd();
 }
 
-/*draw rectangle by calling clRecti with identical arguments*/
-
-void clRecti_1( GLint x1, GLint y1, GLint x2, GLint y2 )
+void clRectLinef( float x1, float y1, float x2, float y2 )
 {
- glRecti( x1, y1, x2, y2 );
+ glBegin(GL_LINE_LOOP);
+ clVertex2f(x1+1, y1+1);
+ clVertex2f(x2-1, y1+1);
+ clVertex2f(x2-1, y2-1);
+ clVertex2f(x1+1, y2-1);
+ glEnd();
 }
+
+
+void clLinef( float x1, float y1, float x2, float y2 )
+{
+ glBegin(GL_LINES);
+ clVertex2f(x1, y1);
+ clVertex2f(x2, y2);
+ glEnd();
+}
+
+
+
