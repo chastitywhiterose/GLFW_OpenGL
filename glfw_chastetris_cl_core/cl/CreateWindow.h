6f4ed3358cb9e3051a0921605c67b89d0cf952d3
Chastity White Rose <chastitywhiterose@gmail.com>
2023-10-05 16:40:50 -0500

    moved opengl variables including shaders
    also changed version to 3.3

--- glfw_chastetris_cl_core/cl/CreateWindow.h
+++ glfw_chastetris_cl_core/cl/CreateWindow.h
@@ -1,7 +1,78 @@
 /*
  This file contains a CreateWindow function which sets up the OpenGL context.
  The goal is to reduce the size of the main.c file and clean up everything.
+ This is important because in OpenGL, creating a Window is only half the battle.
+ An OpenGL context must also be created of a version that the computer supports.
+
+ I am for version 3.3 in my code and go back to the legacy version on my oldest machines.
 */
+
+/*This section has variables that will be used the the rest of my code.*/
+
+ /*
+  geometry to use. these are 3 xyz points (9 floats total) to make a triangle
+  we will draw two triangles to make a complete rectangle, so there are actually 18 floats for both
+  the vertex positions and the vertex colors
+ */
+ GLfloat points[] =
+ {
+  -0.5f,  0.5f, 0.0f,
+   0.5f,  0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+
+   0.5f, -0.5f, 0.0f,
+   0.5f,  0.5f, 0.0f,
+  -0.5f, -0.5f, 0.0f,
+ };
+
+ GLfloat colours[] =
+ {
+  1.0f, 0.0f, 0.0f,
+  0.0f, 1.0f, 0.0f,
+  0.0f, 0.0f, 1.0f,
+
+  0.0f, 1.0f, 1.0f,
+  1.0f, 0.0f, 1.0f,
+  1.0f, 1.0f, 0.0f 
+ };
+
+  GLuint points_vbo;
+  GLuint colours_vbo;
+
+  GLuint vao;
+
+
+  const GLubyte* renderer;
+  const GLubyte* version;
+
+  /* these are the strings of code for the shaders
+  the vertex shader positions each vertex point */
+  const char* vertex_shader =
+    "#version 410\n"
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_colour;"
+"out vec3 colour;"
+    "void main () {"
+	"colour = vertex_colour;"
+	"gl_Position = vec4(vertex_position, 1.0);"
+    "}";
+
+  /* the fragment shader colours each fragment (pixel-sized area of the
+  triangle) */
+  const char* fragment_shader =
+    "#version 330\n"
+"in vec3 colour;"
+"out vec4 frag_colour;"
+    "void main () {"
+	"frag_colour = vec4 (colour, 1.0);"
+    "}";
+
+  /* GL shader objects for vertex and fragment shader [components] */
+  GLuint vert_shader, frag_shader;
+  /* GL shader programme object [combined, to link] */
+  GLuint shader_programme;
+
+/*end of global modern opengl variables*/
 
 int CreateWindow(int width,int height)
 {
@@ -13,16 +84,18 @@ int CreateWindow(int width,int height)
   }
 
   /* Version 4.1 Core is a good default that should run on just about everything. Adjust later to suit project requirements. */
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+/*Version 3.3 is also very good for targeting the first version of modern opengl*/
+
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
   glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
   glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
   window = glfwCreateWindow( width, height, "OpenGL Core", NULL, NULL );
   if ( !window ) {
     fprintf( stderr, "ERROR: could not open window with GLFW3\n" );
-    glfwTerminate();
     return 1;
+    glfwTerminate();
   }
   glfwMakeContextCurrent( window );
 
